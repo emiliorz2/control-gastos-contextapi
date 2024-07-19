54436a89ebec2d83640c55697fabad200e836ce6
emiliorz2 <emiliorz2@icloud.com>
2024-07-18 19:13:40 -0600

    chore: Add initial project files and configurations

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useMemo } from "react"
+import { BudgetForm } from "./components/BudgetForm"
+import { useBudget } from "./hooks/useBudget"
+import { BudgetTracker } from "./components/BudgetTracker"
+
+
+function App() {
+  
+  const {state} = useBudget()
+
+  const isValidBudget = useMemo(() => state.budget> 0, [state.budget])
+
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+          {isValidBudget ? <BudgetTracker/> : <BudgetForm/>}
+      </div>
+    </>
+  )
+}
+
+export default App
